7d229d2d64ce39d73dfb6cd4cb1c6b5d1cc60ff5
unknown <josesaenz095@gmail.com>
2019-09-08 20:17:01 -0300

    Header V.0
    Header, section y footer

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  require_once "templates/header.php";
+?>
+
+<section class="container-fluid">
+  <div class="row">
+    <div class="row">
+      <div class="">
+
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+  require_once "templates/footer.php";
+?>
---
+++ templates/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Radio FMCool</title>
+  </head>
+  <body>
+  <header class="bg-dark">
+    <div class="text-center">
+      <img src="img/logo.png" alt="Logo FMCool">
+    </div>
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Vida Cool</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ranking</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Login</a>
+      </li>
+    </ul>
+  </header>
